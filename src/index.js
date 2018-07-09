ca2a4ab08213c64cc2b129f90ab095f05f142957
FaisalAli <faisalali1901@gmail.com>
2018-07-09 14:35:35 +0530

    removed logger for production

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
 import App from './containers/App';
 import 'tachyons'
 import "./index.css";
 import { searchRobots, requestsRobots } from './reducers/reducers';
 
-const logger = createLogger();
+const middlewares = [];
+if (process.env.NODE_ENV === `development`) {
+	const logger = createLogger();
+	middlewares.push(thunkMiddleware, logger)
+}else{
+	middlewares.push(thunkMiddleware)
+}
 
 const rootReducers = combineReducers({ searchRobots, requestsRobots })
-const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
+const store = createStore(rootReducers, applyMiddleware(...middlewares))
 
 ReactDOM.render(
 	<Provider store={store}>
